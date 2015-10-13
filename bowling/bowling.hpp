818f7e1cdf927c73d27036eaffadc5757468a130
Kuba Borowski <prajmus@gmail.com>
2015-10-13 16:37:20 +0200

    [bowling] Add methods

--- bowling/bowling.hpp
+++ bowling/bowling.hpp
@@ -1,8 +1,11 @@
+/* Copyright (c) 2015, Jakub Borowski */
 #ifndef BOWLING_H_
 #define BOWLING_H_
 
 class Game {
-
+ public:
+     int roll(int pins);
+     int score();
 };
 
 #endif  // BOWLING_H_
