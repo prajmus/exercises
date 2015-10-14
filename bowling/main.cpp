98b32ed0af0f7584c9c8548b2a875ab0db3716e2
Kuba Borowski <prajmus@gmail.com>
2015-10-14 16:20:38 +0200

    [bowling] Added second test case

--- bowling/main.cpp
+++ bowling/main.cpp
@@ -12,9 +12,17 @@ void test_game() {
     BOOST_CHECK_EQUAL(g.score(), 0);
 }
 
+void test_all_ones() {
+    Game g;
+    for (int i = 0; i < 20; i++)
+        g.roll(1);
+    BOOST_CHECK_EQUAL(g.score(), 20);
+}
+
 
 test_suite *init_unit_test_suite(int argc, char *argv[]) {
     test_suite *t = BOOST_TEST_SUITE("String Calculator");
     t->add(BOOST_TEST_CASE(&test_game));
+    t->add(BOOST_TEST_CASE(&test_all_ones));
     return t;
 }
