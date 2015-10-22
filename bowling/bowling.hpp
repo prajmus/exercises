9f48243ce613fb543a8caaf497e9bb7efc24a9c8
Kuba Borowski <prajmus@gmail.com>
2015-10-22 15:16:20 +0200

    [bowling] Refactor score

--- bowling/bowling.hpp
+++ bowling/bowling.hpp
@@ -11,6 +11,9 @@ class Game {
      int current_roll = 0;
      int rolls[21] = {};
      bool is_spare(int frame_index);
+     int sum_in_frames(int frame_index);
+     int spare_bonus(int frame_index);
+     int strike_bonus(int frame_index);
 };
 
 void Game::roll(int pins) {
@@ -22,15 +25,13 @@ int Game::score() {
     int frame_index = 0;
     for (int frame = 0; frame < 10; frame++) {
         if (rolls[frame_index] == 10) {  //strike
-            score += 10 + rolls[frame_index + 1] +
-                     rolls[frame_index + 2];
+            score += 10 + strike_bonus(frame_index);
             frame_index++;
-        }
-        if (is_spare(frame_index)) {  // spare
-            score += 10 + rolls[frame_index+2];
+        } else if (is_spare(frame_index)) {  // spare
+            score += 10 + spare_bonus(frame_index);
             frame_index += 2;
         } else {
-            score += rolls[frame_index] + rolls[frame_index+1];
+            score += sum_in_frames(frame_index);
             frame_index += 2;
         }
     }
@@ -39,6 +40,18 @@ int Game::score() {
 
 bool Game::is_spare(int frame_index) {
     return rolls[frame_index] + rolls[frame_index + 1] == 10;
+}
+
+int Game::sum_in_frames(int frame_index) {
+    return rolls[frame_index] + rolls[frame_index + 1];
+}
+
+int Game::spare_bonus(int frame_index) {
+    return rolls[frame_index + 2];
+}
+
+int Game::strike_bonus(int frame_index) {
+    return rolls[frame_index + 1] + rolls[frame_index + 2];
 }
 
 #endif  // BOWLING_H_
