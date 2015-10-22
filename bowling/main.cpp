216b94bf10be710fb433c16fc701ebb477866903
Kuba Borowski <prajmus@gmail.com>
2015-10-22 13:52:17 +0200

    [bowling] Removed duplicated loop

--- bowling/main.cpp
+++ bowling/main.cpp
@@ -6,16 +6,19 @@
 class BowlingTest : public ::testing::Test {
  public:
      Game g;
+
+    void roll_many(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
 };
 
 TEST_F(BowlingTest, test_game) {
-    for (int i=0; i<20; i++)
-        g.roll(0);
+    roll_many(20, 0);
     EXPECT_EQ(g.score(), 0);
 }
 
 TEST_F(BowlingTest, test_all_ones) {
-    for (int i = 0; i < 20; i++)
-        g.roll(1);
+    roll_many(20, 1);
     EXPECT_EQ(g.score(), 20);
 }
