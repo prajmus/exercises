a9ef12a51af1e5bc93e0097294cb87ba306c5f35
Kuba Borowski <prajmus@gmail.com>
2015-10-22 13:46:26 +0200

    [bowling] Make Game object a Fixture

--- bowling/main.cpp
+++ bowling/main.cpp
@@ -3,15 +3,18 @@
 
 #include "bowling.hpp"
 
-TEST(bowling, test_game) {
-    Game g;
+class BowlingTest : public ::testing::Test {
+ public:
+     Game g;
+};
+
+TEST_F(BowlingTest, test_game) {
     for (int i=0; i<20; i++)
         g.roll(0);
     EXPECT_EQ(g.score(), 0);
 }
 
-TEST(bowling, test_all_ones) {
-    Game g;
+TEST_F(BowlingTest, test_all_ones) {
     for (int i = 0; i < 20; i++)
         g.roll(1);
     EXPECT_EQ(g.score(), 20);
