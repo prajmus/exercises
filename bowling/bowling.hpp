122fee1ed2070d813a24c111ceef81447aacdc8c
Kuba Borowski <prajmus@gmail.com>
2015-10-22 15:18:53 +0200

    [bowling] Check strike method

--- bowling/bowling.hpp
+++ bowling/bowling.hpp
@@ -11,6 +11,7 @@ class Game {
      int current_roll = 0;
      int rolls[21] = {};
      bool is_spare(int frame_index);
+     bool is_strike(int frame_index);
      int sum_in_frames(int frame_index);
      int spare_bonus(int frame_index);
      int strike_bonus(int frame_index);
@@ -24,7 +25,7 @@ int Game::score() {
     int score = 0;
     int frame_index = 0;
     for (int frame = 0; frame < 10; frame++) {
-        if (rolls[frame_index] == 10) {  //strike
+        if (is_strike(frame_index)) {  //strike
             score += 10 + strike_bonus(frame_index);
             frame_index++;
         } else if (is_spare(frame_index)) {  // spare
@@ -40,6 +41,10 @@ int Game::score() {
 
 bool Game::is_spare(int frame_index) {
     return rolls[frame_index] + rolls[frame_index + 1] == 10;
+}
+
+bool Game::is_strike(int frame_index) {
+    return rolls[frame_index] == 10;
 }
 
 int Game::sum_in_frames(int frame_index) {
